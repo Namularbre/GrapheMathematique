439d035c203fdfd684d4cf3bb4f6e31daa02d45e
Namu Sazerat <namulabre@gmail.com>
2022-09-19 16:13:14 +0200

    Ajout de la fn de tri

--- Graphe/Graphe.cs
+++ Graphe/Graphe.cs
@@ -10,11 +10,33 @@ namespace ApplicationGraphe
     {
         public HashSet<Arete> aretes { get; set; }
         public int nombreArette { get; set; }
+        public HashSet<int> sommets { get; set; }
 
         public Graphe(HashSet<Arete> aretes, int nombreArette)
         {
             this.aretes = aretes;
             this.nombreArette = nombreArette;
+            DefinirSommetAPartirDesAretes();
+        }
+
+        private void DefinirSommetAPartirDesAretes()
+        {
+            this.sommets = new HashSet<int>();
+
+            foreach (var arete in this.aretes)
+            {
+                this.sommets.Add(arete.sommetDepart);
+                this.sommets.Add(arete.sommetArrive);
+            }
+
+            IEnumerable<int> sommetsEnOrdre = this.sommets.OrderBy(sommet => sommet);
+            HashSet<int> sommetsTrie = new HashSet<int>();
+
+            foreach (var sommet in sommetsEnOrdre)
+            {
+                sommetsTrie.Add(sommet);
+            }
+            this.sommets = sommetsTrie;
         }
 
         public string VerdString()
@@ -54,6 +76,40 @@ namespace ApplicationGraphe
             }
 
             this.aretes = aretesTrierParPoid;
+        }
+
+        private int AvoirIndexSommet(int sommetCherche)
+        {
+            int iterateur = 0;
+            foreach (int sommet in this.sommets)
+            {
+                if (sommet == sommetCherche)
+                {
+                    return iterateur;
+                }
+                iterateur++;
+            }
+            return -1;
+        }
+
+        public int[,] AvoirMatriceAdjacence()
+        {
+            int[,] matriceAdjacence = new int[this.nombreArette, this.nombreArette];
+
+            for(int iterateurLigne = 0; iterateurLigne < this.nombreArette; iterateurLigne++)
+            {
+                for(int iterateurColonne = 0; iterateurColonne < this.nombreArette; iterateurColonne++)
+                {
+                    matriceAdjacence[iterateurLigne, iterateurColonne] = 0;
+                }
+            }
+
+            foreach (Arete arete in this.aretes)
+            {
+                matriceAdjacence[AvoirIndexSommet(arete.sommetDepart), AvoirIndexSommet(arete.sommetArrive)] = 1;
+            }
+
+            return matriceAdjacence;
         }
     }
 }
--- Graphe/Program.cs
+++ Graphe/Program.cs
@@ -2,23 +2,30 @@
 
 Graphe CreerGraphe()
 {
-    HashSet<Arete> nouvelleArete = new HashSet<Arete>();
-    char reponse = ' ';
-    while (reponse != 'n')
+    HashSet<Arete> nouvelleAretes = new HashSet<Arete>();
+    int reponse = 1;
+    while (reponse != 0)
     {
-        Console.WriteLine("Entrez le sommet d'où part votre arête");
-        int sommetDepart = int.Parse(Console.ReadLine());
-        Console.WriteLine("Entre le sommet d'où arrive votre arête");
-        int sommetArrive = int.Parse(Console.ReadLine());
-        Console.WriteLine("Entrez le poid de votre arête");
-        int poidArete = int.Parse(Console.ReadLine());
-        Console.WriteLine("Continuer? n=non autre=oui");
-        reponse = char.Parse(Console.ReadLine());
+        try
+        {
+            Console.WriteLine("Entrez le sommet d'où part votre arête");
+            int sommetDepart = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre le sommet d'où arrive votre arête");
+            int sommetArrive = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le poid de votre arête");
+            int poidArete = int.Parse(Console.ReadLine());
+            Console.WriteLine("Continuer? n=non autre=oui");
+            reponse = int.Parse(Console.ReadLine());
 
-        nouvelleArete.Add(new Arete(sommetDepart, sommetArrive, poidArete));
+            nouvelleAretes.Add(new Arete(sommetDepart, sommetArrive, poidArete));
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Vous n'avez pas fait une saisi valide !");
+        }
     }
 
-    return new Graphe(nouvelleArete, nouvelleArete.Count);
+    return new Graphe(nouvelleAretes, nouvelleAretes.Count);
 }
 
 Graphe graphe = CreerGraphe();
